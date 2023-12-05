8eb87c61b087867cc669d348702c31d28ca903dd
杨骜捷 <1404670552@qq.com>
2023-12-05 14:19:34 +0800

    放大镜特效完成

--- js/text6.js
+++ js/text6.js
@@ -1,7 +1,9 @@
 var boxsmall = document.getElementById('box-small');
 var boxbig = document.getElementById('box-big');
+var imgsmall = document.getElementById('img-small');
 var mouse = document.getElementById('mouse');
 var imgbig = document.getElementById('img-big');
+
 function getMousePos(e) {
     var rect = boxsmall.getBoundingClientRect();
     return {
@@ -9,16 +11,26 @@ function getMousePos(e) {
         y: e.clientY - rect.top
     };
 }
-boxsmall.onmouseover = function(e) {
+
+boxsmall.onmousemove = function(e) {
     mouse.style.top = getMousePos(e).y + 'px';
     mouse.style.left = getMousePos(e).x + 'px';
     mouse.style.display = 'block';
     boxbig.style.display = 'block';
-    const imgLeft = getMousePos(e).x < 600 ? getMousePos(e).x * 2 : getMousePos(e).x * 2 - 600;
-    imgbig.style.left = imgLeft + 'px';
-    imgbig.style.top = getMousePos(e).y *2 + 'px';
+
+    const imgLeft = (getMousePos(e).x < 400) ? getMousePos(e).x * 2 : getMousePos(e).x * 2 - 300;
+    const imgTop = (getMousePos(e).y <400) ? getMousePos(e).y * 2 : getMousePos(e).y * 2 - 300;
+
+    imgbig.style.left = -imgLeft + 'px';
+    imgbig.style.top = -imgTop + 'px';
 }
-boxsmall.onmouseout = function(e) {
+
+boxsmall.onmouseleave = function() {
     mouse.style.display = 'none';
     boxbig.style.display = 'none';
 }
+/*问提总结
+应该使用 boxsmall 而不是 imgsmall 来获取小盒子的边界矩形。
+对于位置算法的设计不应该以大图为标准,应该以小图为标准。不然会导致大图的位置不对。
+仍然存在图片过渡边界时过度不平滑的问题暂时无法解决
+* */
